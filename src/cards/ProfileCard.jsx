b2e86608089f2864d9537a17060d836ed57a77ca
Puth Vang <35241506+PuthVang@users.noreply.github.com>
2023-07-27 11:02:54 -0700

    Updated dependencies and added in portrait and GitHub icon

--- src/cards/ProfileCard.jsx
+++ src/cards/ProfileCard.jsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
-import {Box, Card, Grid, Typography} from "@mui/material";
+import {Box, Card, CardMedia, Grid, Typography} from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function MediaControlCard() {
 
     return (
+        <>
         <Box p={1} sx={{ maxWidth: '100vh'}}>
             <Card sx={{ display: 'flex', maxWidth: 1500, maxHeight: 700}}>
                 <Box sx={{ display: 'flex', flexDirection: 'column' }}>
@@ -21,7 +24,24 @@ export default function MediaControlCard() {
                         </Typography>
                     </Grid>
                 </Box>
+
+                    <Grid>
+                        <CardMedia
+                            component="img"
+                            alt="Portrait"
+                            src="././portrait.png"
+                            sx={{minHeight: 200, maxHeight: 500}}
+                        />
+                        <Grid style={{ display: "flex" }}>
+                            <Box sx={{ m: 1 }}>
+                                <IconButton>
+                                    <GitHubIcon onClick={() => window.open('https://github.com/Unalphabetical/', '_blank')} />
+                                </IconButton>
+                            </Box>
+                        </Grid>
+                    </Grid>
             </Card>
         </Box>
+        </>
 );
 }
