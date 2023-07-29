83b604ddc9dd0e06dc401cf24f3cd181a546979a
Puth Vang <35241506+PuthVang@users.noreply.github.com>
2023-07-28 20:31:52 -0700

    Theming works for the whole website now.

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,49 @@ import React, { useState } from 'react';
 import './App.css';
 import ProfileCard from './cards/ProfileCard.jsx';
 import ThemeButton from './buttons/ThemeButton.jsx';
-import {createTheme, CssBaseline, Grid, ThemeProvider} from '@mui/material';
+import {Box, Button, createTheme, CssBaseline, Grid, ThemeProvider} from '@mui/material';
+
+const getDesignTokens = (mode) => ({
+  palette: {
+    mode,
+    ...(mode === 'light'
+        ? {
+          background: {
+            default: '#fafafa'
+          },
+          text: {
+            primary: '#050505',
+            secondary: '#c0074e',
+            tertiary: '#fdcee0',
+            accent: '#f70864'
+          },
+        }
+        : {
+          background: {
+            default: '#050505'
+          },
+          text: {
+            primary: '#fafafa',
+            secondary: '#c0074e',
+            tertiary: '#310214',
+            accent: '#fa6ba2'
+          },
+        }),
+  },
+});
 
 function App() {
 
-  const [themeMode, setThemeMode] = useState("dark")
+  const [themeMode, setThemeMode] = useState('dark')
 
-  const theme = createTheme({
-    palette: {
-      mode: themeMode,
-    },
-  });
+  const theme = React.useMemo(() => createTheme(getDesignTokens(themeMode)), [themeMode]);
 
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
 
-        <Grid flexGrow={0} sx={{ position: "fixed", top: 0, right: 0, zIndex: 2000 }}>
+        <Grid flexGrow={0} sx={{ position: 'fixed', top: 0, right: 0, zIndex: 2000 }}>
           <ThemeButton label={themeMode} setTheme={setThemeMode}></ThemeButton>
         </Grid>
 
--- src/cards/ProfileCard.jsx
+++ src/cards/ProfileCard.jsx
@@ -1,47 +1,56 @@
 import * as React from 'react';
-import {Box, Card, CardMedia, Grid, Typography} from '@mui/material';
+import {Box, Card, CardMedia, Grid, styled, Typography} from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import GitHubIcon from '@mui/icons-material/GitHub';
-
 export default function MediaControlCard() {
-
-    return (
-        <>
-        <Box p={1} sx={{ maxWidth: '100vh'}}>
-            <Card sx={{ display: 'flex', maxWidth: 1500, maxHeight: 700}}>
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <Grid
-                        container
-                        spacing={0}
-                        alignItems="center"
-                        justify="center"
-                    >
-                        <Typography component="div" align="left">
-                            <Box sx={{ fontSize: 'h4.fontSize', m: 2 }}>Hi, I'm Puth.</Box>
-                            <Box sx={{ fontSize: 'h4.fontSize', m: 2 }}>A fullstack developer.</Box>
-                            <Box sx={{ fontSize: 'h7.fontSize', m: 2 }}>I'm currently learning React alongside software languages like Java</Box>
-                            <Box sx={{ fontSize: 'h7.fontSize', m: 2 }}>and studying at California State University, Sacramento as a Computer Science major</Box>
-                        </Typography>
-                    </Grid>
-                </Box>
-
-                    <Grid>
-                        <CardMedia
-                            component="img"
-                            alt="Portrait"
-                            src="././portrait.png"
-                            sx={{minHeight: 200, maxHeight: 500}}
-                        />
-                        <Grid style={{ display: "flex" }}>
-                            <Box sx={{ m: 1 }}>
-                                <IconButton>
-                                    <GitHubIcon onClick={() => window.open('https://github.com/Unalphabetical/', '_blank')} />
-                                </IconButton>
-                            </Box>
-                        </Grid>
-                    </Grid>
-            </Card>
-        </Box>
-        </>
-);
+  return (
+    <>
+      <Box p={1} sx={{ maxWidth: '100vh'}}>
+        <Card sx={{ display: 'flex', maxWidth: 1500, maxHeight: 700}}>
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Grid
+              container
+              spacing={0}
+              alignItems='center'
+              justify='center'
+            >
+              <Typography component='div' align='left' sx={{ m: 2 }}>
+                <Grid container sx={{ alignItems: 'left', gap: '0.7rem' }}>
+                  <Box sx={{ color: 'text.primary', fontSize: 'h4.fontSize'}}>Hi, I'm </Box>
+                  <Box sx={{ color: 'text.secondary', fontSize: 'h4.fontSize' }}>Puth.</Box>
+                </Grid>
+                <Box sx={{ color: 'text.secondary', fontSize: 'h4.fontSize' }}>A fullstack developer.</Box>
+                <Grid>
+                  <Grid container sx={{ alignItems: 'left', gap: '0.3rem', pt: 1 }}>
+                    <Typography sx={{ fontSize: 'h7.fontSize' }}>
+                      I'm currently learning <Typography display="inline" sx={{ color: 'text.secondary' }}>React</Typography> alongside software languages like <Typography display="inline" sx={{ color: 'text.secondary' }}>Java</Typography>
+                    </Typography>
+                    <Typography sx={{ fontSize: 'h7.fontSize' }}>
+                      and studying at <Typography display="inline" sx={{ color: 'text.secondary' }}>California State University, Sacramento</Typography> as a <Typography display="inline" sx={{ color: 'text.secondary'}}>Computer Science major</Typography>.
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Typography>
+            </Grid>
+          </Box>
+          <Grid>
+            <CardMedia
+              component='img'
+              alt='Portrait'
+              src='././portrait.png'
+              sx={{minWidth: 150, minHeight: 200, maxHeight: 500}}
+            />
+            <Grid style={{ display: 'flex' }}>
+              <Box sx={{ m: 1 }}>
+                <IconButton>
+                  <GitHubIcon onClick={() =>
+                    window.open('https://github.com/Unalphabetical/', '_blank')} />
+                </IconButton>
+              </Box>
+            </Grid>
+          </Grid>
+        </Card>
+      </Box>
+    </>
+  );
 }
