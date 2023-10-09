ac88020bc7cc0042ff71adfd96613a2372d454b6
Puth Vang <35241506+PuthVang@users.noreply.github.com>
2023-10-08 20:54:12 -0700

    Vertically and horizontally center the website for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
           <ThemeButton label={themeMode} setTheme={setThemeMode}></ThemeButton>
         </Grid>
 
-        <Grid sx={{display: 'flex'}}>
+        <Grid sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '90vh'}}>
           <ProfileCard></ProfileCard>
         </Grid>
 
