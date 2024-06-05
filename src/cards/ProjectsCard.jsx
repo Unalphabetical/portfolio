4443064ec69e7842dfe9dd53c5d1d3024e55cc9b
Unalphabetical <puthvangjfk@gmail.com>
2024-06-04 23:45:37 -0700

    Added in main menu image

--- src/cards/ProjectsCard.jsx
+++ src/cards/ProjectsCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Box, Card, Tab, Tabs, Typography} from '@mui/material';
+import {Box, Card, Grid, Tab, Tabs, Typography} from '@mui/material';
 
 export default function ProjectsCard() {
     const [value, setValue] = useState(0);
@@ -30,12 +30,27 @@ export default function ProjectsCard() {
                         </Box>
                         <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                             {value === 0 &&
+                                <Grid>
                                 <Typography>
                                     Snake game made with Android Studio for mobile during California State University, Sacramento
                                     in one of my Computer Science class: Object-Oriented Computer Graphics Programming.
                                     It features background music, gamepad controls, a leaderboard system, a delete
-                                    button incase of data corruption, and multiple apples.
-                                </Typography>}
+                                    button in case of data corruption, and multiple apples.
+                                </Typography>
+                                <Box
+                                    component="img"
+                                    sx={{
+                                        height: '100%',
+                                        width: '95%',
+                                        minHeight: '100%',
+                                        minWidth: '40%',
+                                        padding: '16px 0'
+                                    }}
+                                    alt="Main menu of the Snake game"
+                                    src="../../snake.png"
+                                />
+                                </Grid>
+                            }
                         {value === 1 && <div>Content for Tab 2</div>}
                         </Box>
                     </Card>
