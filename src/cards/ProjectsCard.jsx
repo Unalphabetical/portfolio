b19355d4d969bbd76e6da087ab4c3b9587781303
Unalphabetical <puthvangjfk@gmail.com>
2024-06-04 21:41:54 -0700

    Added more info to projects section

--- src/cards/ProjectsCard.jsx
+++ src/cards/ProjectsCard.jsx
@@ -1,16 +1,42 @@
-import React from 'react';
-import {Box, Card, Typography} from '@mui/material';
+import React, {useState} from 'react';
+import {Box, Card, Grid, Tab, Tabs, Typography} from '@mui/material';
 
 export default function ProjectsCard() {
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return (
         <Box p={1} sx={{ display: 'flex', flexDirection: 'column', maxWidth: '1000px' }}>
             <Card sx={{ display: 'flex', flexDirection: 'row', width: '100%', height: '100%' }}>
+
                 <Box>
                     <Typography component='div' align='left' sx={{ m: 1.5 }}>
-                        <Box sx={{ color: 'text.primary', fontSize: 'h5.fontSize'}}>Projects</Box>
+                        <Box sx={{ color: 'text.primary', fontSize: 'h5.fontSize' }}>Projects</Box>
                     </Typography>
+                    <Card sx={{ display: 'flex', flexDirection: 'row', width: '100%', height: '100%' }}>
+                        <Box sx={{ minWidth: '30%' }}>
+                            <Tabs
+                                orientation="vertical"
+                                variant="scrollable"
+                                value={value}
+                                onChange={handleChange}
+                            >
+                                <Tab label="Tab 1" />
+                                <Tab label="Tab 2" />
+                            </Tabs>
+                        </Box>
+                        <Box>
+                            <Grid container sx={{ alignItems: 'left', gap: '0.7rem' }}>
+                                {value === 0 && <div>Content for Tab 1</div>}
+                                {value === 1 && <div>Content for Tab 2</div>}
+                            </Grid>
+                        </Box>
+                    </Card>
                 </Box>
+
             </Card>
         </Box>
     );
