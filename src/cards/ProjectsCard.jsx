5850d04927bf63987028e4f3781781f17483fb58
Unalphabetical <puthvangjfk@gmail.com>
2024-06-04 23:07:15 -0700

    Finished basic details for the first tab

--- src/cards/ProjectsCard.jsx
+++ src/cards/ProjectsCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Card, Tab, Tabs, Typography } from '@mui/material';
+import {Box, Card, Tab, Tabs, Typography} from '@mui/material';
 
 export default function ProjectsCard() {
     const [value, setValue] = useState(0);
@@ -24,13 +24,19 @@ export default function ProjectsCard() {
                                 value={value}
                                 onChange={handleChange}
                             >
-                                <Tab label="Tab 1" />
+                                <Tab label="Snake" />
                                 <Tab label="Tab 2" />
                             </Tabs>
                         </Box>
                         <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                            {value === 0 && <div>Content for Tab 1</div>}
-                            {value === 1 && <div>Content for Tab 2</div>}
+                            {value === 0 &&
+                                <Typography>
+                                    Snake game made with Android Studio for mobile during California State University, Sacramento
+                                    in one of my Computer Science class: Object-Oriented Computer Graphics Programming.
+                                    It features background music, gamepad controls, a leaderboard system, a delete
+                                    button incase of data corruption, and multiple apples.
+                                </Typography>}
+                        {value === 1 && <div>Content for Tab 2</div>}
                         </Box>
                     </Card>
                 </Box>
