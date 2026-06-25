7f8b1f804ff5975145b878cfc2186003677653b1
Unalphabetical <35241506+Unalphabetical@users.noreply.github.com>
2026-06-24 17:59:39 -0700

    Add social links data structure

---
+++ src/data/socials.js
@@ -0,0 +1,18 @@
+export const socials = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/Unalphabetical',
+  },
+  {
+    name: 'Website',
+    url: 'https://puthvang.com',
+  },
+  {
+    name: 'Email',
+    url: 'mailto:vangputh@gmail.com',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/puthvang/',
+  },
+];
